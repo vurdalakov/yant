9955d0f7037c4be69ae6b7e44b52a73cedcf8c80
vurdalakov <vurdalakov@localhost>
2011-12-02 14:24:10 +0200

    jar removed, icons and options dlg improved

--- src/1_make.bat
+++ src/1_make.bat
@@ -3,32 +3,19 @@
 echo *** 1. Initializing
 
 set addon=yant
-set version=0.1.1
+set version=0.1.2
 
 set zipper="C:\Program Files\WinRAR\WinRAR.exe" a -afzip -r 
 
-set jar=%addon%.jar
 set xpi=%addon%_%version%.xpi
-
-set jar_path=install
 
 echo *** 2. Cleaning
 
 if exist %xpi% del %xpi%
-if exist %jar_path%\%jar% del %jar_path%\%jar%
 
-echo *** 3. Creating [%jar%]
+echo *** 3. Creating [%xpi%]
 
 cd source
-%zipper% %jar% *.*
-move %jar% ..\%jar_path%
-cd ..
-
-echo *** 4. Creating [%xpi%]
-
-cd install
 %zipper% %xpi% *.*
 move %xpi% ..
 cd ..
-
-del %jar_path%\%jar%
